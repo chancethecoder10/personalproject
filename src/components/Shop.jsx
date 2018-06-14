615041c24dcbd0838af5ead94e129f8cf8e0bcd1
“Jonathan <jonwhite1993@gmail.com>
2018-06-14 11:39:47 -0600

    added shop layout

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Jumbotron } from 'react-bootstrap'
+import '../styles/Shop.css'
 
-export default function Shop(){
-    return(
-        <div>
-            <p>Shop</p>
-        </div> 
-    )
+class Shop extends Component {
+    render() {
+        return (
+
+            <Jumbotron className='shopotron'>
+                <div id="showgrid">
+                    <div className="row">
+                        <div className="column"></div>
+                        <div className="column"></div>
+                        <div className="column"></div>
+                    </div>
+                    <div className="row">
+                        <div className="column"></div>
+                        <div className="column"></div>
+                        <div className="column"></div>
+                    </div>
+                    <div className="row">
+                        <div className="column"></div>
+                        <div className="column"></div>
+                        <div className="column"></div>
+                    </div>
+                </div>
+            </Jumbotron>
+        )
+    }
 }
+export default Shop
